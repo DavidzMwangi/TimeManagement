834b3a387404b8ed7696dbdeaa17281fd9309715
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2019-06-21 22:30:26 +0300

    completed api testing

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('userer','ProfileController@authUser');
 
 Route::group(['namespace'=>'Api'],function () {
 
@@ -28,3 +29,25 @@ Route::group(['namespace'=>'Api'],function () {
     });
 
 });
+    Route::group(['middleware'=>'auth:api'],function () {
+
+        //admin
+        Route::get('get_all_users','UserController@getAllUsers')->name('get_all_users');
+        Route::post('save_new_user','UserController@saveNewUser')->name('save_new_user');
+        Route::get('delete_user/{user}','UserController@deleteUser')->name('delete_user');
+
+
+        //manager
+
+        Route::get('get_all_regular_users','UserController@getAllRegularUsers')->name('get_all_regular_users');
+        Route::post('save_new_user','UserController@saveNewUser')->name('save_new_user');
+        Route::get('delete_user/{user}','UserController@deleteUser')->name('delete_user');
+
+
+        //regular user
+            Route::get('get_user_tasks','TaskController@getUserTasks')->name('get_user_tasks');
+            Route::post('save_new_task','TaskController@saveNewTask')->name('save_new_task');
+            Route::get('delete_task/{task}','TaskController@deleteTask')->name('delete_task');
+            Route::get('mark_as_complete_task/{task}','TaskController@markAsComplete')->name('mark_as_complete_task');
+    });
+
